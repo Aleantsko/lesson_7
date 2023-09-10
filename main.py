1a17a9c96eccdd531054caa8b8dc46a9893d1c40
Aleantsko <135431196+Aleantsko@users.noreply.github.com>
2023-09-10 11:32:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
         answer = []
         for j in range(1, num_columns + 1):
             answer.append(str(operation(i, j)))
-        print(''.join(f'{e:<4}' for e in answer))
+        print(''.join(f'{e:2} ' for e in answer))
+
 
 
 print_operation_table(lambda x, y: x * y)
